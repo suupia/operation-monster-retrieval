3aba8839fdf296d5ea5e5717208f2157fe0752bd
suupia <morita777k@icloud.com>
2022-01-16 10:09:12 +0900

    StatusCanvasの原型を作った。

--- Assets/Scripts/UI/StatusCanvasMGR.cs
+++ Assets/Scripts/UI/StatusCanvasMGR.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class StatusCanvasMGR : MonoBehaviour
 {
     [SerializeField] Text nameText;
-    [SerializeField] Text characterTypeIDText;
     [SerializeField] Text levelText;
     [SerializeField] Text hpText;
     [SerializeField] Text atkText;
@@ -30,15 +29,14 @@ public class StatusCanvasMGR : MonoBehaviour
     void UpdateStatusCanvas(int characterTypeID)
     {
         nameText.text = GameManager.instance.GetCharacterDatabase(characterTypeID).GetCharacterName();
-        characterTypeIDText.text = "ID:"+ GameManager.instance.GetCharacterDatabase(characterTypeID).GetCharacterTypeID().ToString();
-        levelText.text = "LEVEL:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetLevel().ToString();
-        hpText.text ="HP:"+ GameManager.instance.GetCharacterDatabase(characterTypeID).GetMaxHp().ToString();
-        atkText.text = "ATK:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetAtk().ToString();
-        attackIntervalText.text = "Attack Interval:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetAttackInterval().ToString();
-        attackRangeText.text = "Attack Range:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetAttackRange().ToString();
-        spdText.text = "SPEED:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetSpd().ToString();
+        levelText.text = " LV :"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetLevel().ToString();
+        costText.text = "Cost:" + GameManager.instance.GetCharacterDatabase(characterTypeID).GetCost().ToString();
+        hpText.text ="  HP  :"+ GameManager.instance.GetCharacterDatabase(characterTypeID).GetMaxHp().ToString();
+        atkText.text = " ATK :"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetAtk().ToString();
+        spdText.text = " SPD :" + GameManager.instance.GetCharacterDatabase(characterTypeID).GetSpd().ToString();
+        attackIntervalText.text = "ATK Interval:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetAttackInterval().ToString();
+        attackRangeText.text = " ATK Range :"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetAttackRange().ToString();
         coolTimeText.text = "Cool Time:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetCoolTime().ToString();
-        costText.text = "Cost:"+GameManager.instance.GetCharacterDatabase(characterTypeID).GetCost().ToString();
     }
 
 }
